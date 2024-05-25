38bc7e203486731357724e4fa4b5ed7bfab9c161
Mitch Toran <mitchellht34@gmail.com>
2024-05-25 13:20:42 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let palindromic = true;
+  if(word.length > 1){
+    for(let i = 0; i < word.length; i++){
+      if(word.charAt(i) !== word.charAt(word.length - (i + 1))){
+        palindromic = false;
+      }
+    }
+  }
+  return palindromic;
 }
 
 /* 
